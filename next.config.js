9366d0e4780986e8e563070199136fb9c8dff46b
Roman Tuomisto <roman.tuomisto@teliacompany.com>
2021-12-23 15:46:07 +0200

    update next conf

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 // next.config.js
-const withTM = require("next-transpile-modules")(["@teliads/components", "@stencil/core"]); // pass the modules you would like to see transpiled
+const withTM = require("next-transpile-modules")(["@teliads/components", "@stencil/core"]); 
+// pass the modules you would like to see transpiled
 
 module.exports = withTM({
   reactStrictMode: true,
